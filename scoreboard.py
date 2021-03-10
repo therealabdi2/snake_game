b8811687d12f405ac1bfd8f1ac6b31ab0d3ede72
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-03-11 01:54:26 +0500

    Added initial files

---
+++ scoreboard.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+        self.penup()
+        self.goto(0, 280)
+        self.pendown()
+        self.update()
+
+    def update(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 12, "normal"))
+
+    def reset_scoreboard(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update()
+
+    def inc_score(self):
+        self.score += 1
+        self.update()
